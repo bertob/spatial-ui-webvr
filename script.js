4b4d932d6ab35291f043438176c8621e19157661
Tobias Bernard <tobi@cg-lab>
2017-03-21 18:10:50 +0100

    [add] timestamps every 8 cards

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ document.getElementById("right-hand").addEventListener("triggerup", function() {
 
 
 for (var i=0; i<40; i++) {
-  var el = document.createElement("a-entity");
-  el.setAttribute("card", "list_position: " + i);
+  var card = document.createElement("a-entity");
+  card.setAttribute("card", "list_position: " + i);
 
   var avatar = document.createElement("a-circle");
   avatar.setAttribute("radius", 0.03);
@@ -20,6 +20,21 @@ for (var i=0; i<40; i++) {
   avatar.setAttribute("position", {x: -0.105, y: 0, z: 0.0015});
   avatar.setAttribute("material", "side:double");
 
-  el.appendChild(avatar);
-  document.getElementById("scene").appendChild(el);
+  card.appendChild(avatar);
+
+  if ( i%8 === 0 ) {
+    var h = Math.floor(i/2) + 1;
+    // var min = Math.floor(Math.random()*60);
+    // var str = h + "h " + min + "min ago";
+    var str = h + "h ago";
+
+    var timestamp = document.createElement("a-entity");
+    timestamp.setAttribute("position", {x: 0.2, y: -0.04, z: 0.0015});
+    timestamp.setAttribute("text-geometry", "value: " + str + "; size: 0.02; height: 0.001;");
+    timestamp.setAttribute("material", "color: #333");
+
+    card.appendChild(timestamp);
+  }
+
+  document.getElementById("scene").appendChild(card);
 }
