8203053f331fd59c4e9c5f9de099050d3dee3a63
Tobias Bernard <tobi@cg-lab>
2017-03-21 17:18:48 +0100

    [minor] fix avatar number

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ for (var i=0; i<40; i++) {
 
   var avatar = document.createElement("a-circle");
   avatar.setAttribute("radius", 0.03);
-  var id = Math.floor(Math.random()*12) + 1;
+  var id = Math.floor(Math.random()*11) + 1;
   avatar.setAttribute("src", "#avatar" + id);
   avatar.setAttribute("color", "#fff");
-  avatar.setAttribute("position", {x: -0.105, y: 0, z: 0.002});
+  avatar.setAttribute("position", {x: -0.105, y: 0, z: 0.0015});
   avatar.setAttribute("material", "side:double");
 
   el.appendChild(avatar);
